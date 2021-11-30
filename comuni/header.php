1f8a1ba9f168e5e24ed58c1a5d862447f9874e45
MrSpleen3 <93833269+MrSpleen3@users.noreply.github.com>
2021-11-30 17:22:11 +0100

    Add files via upload

---
+++ comuni/header.php
@@ -0,0 +1,7 @@
+<div class="header">
+ <div class="nomesito">
+  <h1><i>Prefelibro</i> </h1>
+ </div>
+ <div class="barraricerca"><input type="text" placeholder="cerca.."></div>
+ <div class= "areaaccedi"><img src="user1.png" alt="user"> </div>
+</div>
---
+++ comuni/nav.php
@@ -0,0 +1,7 @@
+<div class="navbar">
+  <a href="#">Home</a>
+  <a href="#">Cerca</a>
+  <a href="#">Registrati</a>
+  <a href="#">Accedi</a>
+   <a href="#">Carrello</a>
+</div>
