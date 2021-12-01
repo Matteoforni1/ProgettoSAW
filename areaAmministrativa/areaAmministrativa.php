eadc9b5c00676633a448bccf581eb731aa817a5e
MrSpleen3 <93833269+MrSpleen3@users.noreply.github.com>
2021-12-01 12:56:06 +0100

    Update areaAmministrativa.php

--- areaAmministrativa/areaAmministrativa.php
+++ areaAmministrativa/areaAmministrativa.php
@@ -6,21 +6,22 @@
 </head>
 <body>
 <?php
-         require('../comuni/header.php');// includo la parte comune
-		 require('../comuni/nav.php');  //  
 		 session_start();
 	     if (!isset($_SESSION["admin"])){
 			 header('Location: adiminLogin.php');
 			 exit();
 		 }
 		 else {
+			 require('../comuni/header.php');    
+		         require('../comuni/nav.php');       
 			 echo "<div class='container'>";
 			  echo "<a href='allUsers.php'>Utenti</a>";
 			  echo "<a href='allBooks.php'>Libri</a>";
 			  echo "<a href='allTranactions.php'>Transazioni</a>";
 			 echo "</div>";
+			 require('../comuni/footer.php');
 		 }
-         require('../comuni/footer.php');
+        
 ?>
 </body>
 </html>
