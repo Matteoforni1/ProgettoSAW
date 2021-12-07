5b0a7338b24ba76a3340defb26edfce1224fb38d
MrSpleen3 <93833269+MrSpleen3@users.noreply.github.com>
2021-12-07 11:24:19 +0100

    Update areaAmministrativa.php

--- areaAmministrativa/areaAmministrativa.php
+++ areaAmministrativa/areaAmministrativa.php
@@ -6,22 +6,31 @@
 </head>
 <body>
 <?php
-		 session_start();
-	     if (!isset($_SESSION["admin"])){
-			 header('Location: adiminLogin.php');
-			 exit();
-		 }
-		 else {
-			 require('../comuni/header.php');    
-		         require('../comuni/nav.php');       
-			 echo "<div class='container'>";
-			  echo "<a href='allUsers.php'>Utenti</a>";
-			  echo "<a href='allBooks.php'>Libri</a>";
-			  echo "<a href='allTranactions.php'>Transazioni</a>";
-			 echo "</div>";
-			 require('../comuni/footer.php');
-		 }
-        
+	session_start();
+	if (!isset($_SESSION["adminid"])){
+		header('Location: adiminLogin.php');
+		exit();
+	}
+	else{
+		$lastactivity=$_SESSION["last_activity"];
+		$lastactivity= time() - $lastactivity;
+		if($lastactivity > 1800) {
+			session_destroy();
+			header('Location:login.php');
+			exit();
+		}
+		else {
+			$_SESSION["last_activity"]=time();
+			require('../comuni/header.php');
+			require('../comuni/nav.php'); 
+			echo "<div class='container'>";
+			 echo "<a href='allUsers.php'>Utenti</a>";
+			 echo "<a href='allBooks.php'>Libri</a>";
+			 echo "<a href='allTranactions.php'>Transazioni</a>";
+			echo "</div>";
+			require('../comuni/footer.php');
+		}
+	}	
 ?>
 </body>
 </html>
