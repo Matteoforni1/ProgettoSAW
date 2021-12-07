bef4e457461ade34f0aa74400d9b20ad7adbaeba
MrSpleen3 <93833269+MrSpleen3@users.noreply.github.com>
2021-12-07 11:23:00 +0100

    Update allUsers.php

--- areaAmministrativa/allUsers.php
+++ areaAmministrativa/allUsers.php
@@ -6,20 +6,34 @@
 </head>
 <body>
 <?php 
-		 session_start();
-	     if (!isset($_SESSION["admin"])){
-			 header('Location: adminLogin.php');
-			 exit();
-		 }
-		 else {
-			 require('../comuni/header.php');    
-		         require('../comuni/nav.php');       
-			 require('../DB_connections/db_admin_access.php');
-			 $conn=superadmin_access();
-			 $query="SELECT id, nome, cognome, email, ban FROM utente";                          //tabella utente necessita attributo boolean ban
-			 $result=mysqli_query($conn,$query);                                                 //per bannare utenti senza eliminarli dal db
-			echo "<table><tr><th>NOME</th><th>COGNOME</th><th>EMAIL</th><th>BAN</th></tr>";      //come vuole la prof
-			while($row = mysqli_fetch_array($risultato)) {
+	session_start();
+	if (!isset($_SESSION["adminid"])){
+		header('Location: adiminLogin.php');
+		exit();
+	}
+	else{
+		$lastactivity=$_SESSION["last_activity"];
+		$lastactivity= time() - $lastactivity;
+		if($lastactivity > 1800) {
+			session_destroy();
+			header('Location:login.php');
+			exit();
+		}
+		else {
+			$_SESSION["last_activity"]=time();
+			require('../comuni/header.php');    
+		    require('../comuni/nav.php');
+			if (isset($_SESSION["errore"])){
+				echo('<script type="text/javascript"> alert("'.$_SESSION["errore"].'")</script>');
+				unset($_SESSION["errore"]);
+			}
+			require('../DB_connections/db_admin_access.php');
+			$conn=superadmin_access();
+			$query="SELECT id, nome, cognome, email, ban FROM utente";
+			$result=mysqli_query($conn,$query);
+			echo "<table><tr><th>NOME</th><th>COGNOME</th><th>EMAIL</th><th>BAN</th></tr>";
+			while($row = mysqli_fetch_array($result)) {
+				$id= $row['id'];
 				$nome = trim($row['nome']);
 				$cognome = trim($row['cognome']);
 				$email = trim($row['email']);
@@ -27,7 +41,7 @@
 				$email=strtolower($email);
 				echo "<tr><td>" .$nome. "</td><td>" .$cognome. "</td><td>" .$email. "</td>";
 				if (!$ban) {
-					echo "<td><a href='ban.php?id=".$email."'>Ban</a></td>";
+					echo "<td><a href='ban.php?id=".$id."'>Ban</a></td>";
 				}
 				else {
 					echo " <td> Utente bannato </td>";
@@ -37,8 +51,8 @@
 			echo "</table>";
 			mysqli_close($conn);
 			require('../comuni/footer.php');
-		 }
-	
+		}
+	}
 ?>
 </body>
 </html>
