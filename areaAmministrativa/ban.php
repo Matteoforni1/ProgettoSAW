f87165e0e39f68b194e3b77f49394da61d755769
MrSpleen3 <93833269+MrSpleen3@users.noreply.github.com>
2021-12-07 11:19:57 +0100

    Update ban.php

--- areaAmministrativa/ban.php
+++ areaAmministrativa/ban.php
@@ -1,18 +1,40 @@
 <?php
-if (!isset($_SESSION["admin"])){
-			 header('Location: adiminLogin.php');
-			 exit();
-		}
-		else {
-			require('../DB_connections/db_admin_access.php');
-			$conn= super_admin_access();
-			$email = trim($_GET['id']);
-			$email=strtolower($email);
-			$query = "UPDATE utente SET ban= 1 WHERE email = '$email'";
-			$res = mysqli_query($conn,$query);
-			echo "utente bannato"; 
-			mysqli_close($conn);
-			header('Location: allUser.php');
+	session_start();
+	if (!isset($_SESSION["adminid"])){
+		header('Location: adiminLogin.php');
+		exit();
+	}
+	else{
+		$lastactivity=$_SESSION["last_activity"];
+		$lastactivity= time() - $lastactivity;
+		if($lastactivity > 1800) {
+			session_destroy();
+			header('Location:login.php');
 			exit();
 		}
+		else {
+			$_SESSION["last_activity"]=time();
+			$id = $_GET['id'];
+			$adminid=$_SESSION["adminid"];
+			$pattern="/^[0-9]{11}$/";
+			if (preg_match($pattern,$id)&&preg_match($pattern,$adminid)) {
+				$data=date("d/m/Y");
+				require('../DB_Connections/webadmin_access.php');
+				$conn= webadmin_access();
+				$query = "INSERT INTO ban (id_utente,id_amm,data) VALUES ('$id','$adminid','$data')";
+				$res = mysqli_query($conn,$query);
+				echo "utente bannato"; 
+				mysqli_close($conn);
+				header('Location: allUser.php');
+				exit();
+			}
+			else {
+				$_SESSION_["errore"]="vi è stato un errore nel ban,ripetere l'operazione";
+				mysqli_close($conn);
+				header("Location: allUsers.php");
+				exit();
+			}
+				
+		}
+	}
 ?>
