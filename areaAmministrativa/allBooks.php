8ce5f0c9322d93aef5490ae86385455f65a9374f
MrSpleen3 <93833269+MrSpleen3@users.noreply.github.com>
2021-12-20 10:36:12 +0100

    Update allBooks.php

--- areaAmministrativa/allBooks.php
+++ areaAmministrativa/allBooks.php
@@ -2,13 +2,13 @@
 <html>
 <head>
     <title>allBooks</title>     
-	<link href="stileSaw.css" rel="stylesheet">
+	<link href="stileSawpr.css" rel="stylesheet">
 </head>
 <body>
 <?php
-	session_start();
+session_start();
 	if (!isset($_SESSION["adminid"])){
-		header('Location: adiminLogin.php');
+		header('Location:login.php');
 		exit();
 	}
 	else{
@@ -25,11 +25,11 @@
 			require('comuni/header.php');    
 		    require('comuni/nav.php');    
 			require('DB_connections/webadmin_access.php');	
-			$conn=webadmin_access_access();
+			$conn=webadmin_access();
 			$query="SELECT ISBN, nome, autori, costo FROM libro";
 			$result=mysqli_query($conn,$query);
 			echo "<div class='tabella'>";
-			echo" <div class='add'><a href='insertBook.php'>aggiungi un libro...</a></div>";
+			echo" <div class='add'><a href='insert2Book.php'>aggiungi un libro...</a></div>";
 			echo "<div class='riga' id='titolo'>";
 			echo "<div class='cella'>ISBN</div>";
 			echo "<div class='cella'>Nome</div>";
@@ -45,15 +45,16 @@
 				echo "<div class='cella'>" .$ISBN. "</div><div class='cella'>" .$nome. "</div><div class='cella'>" .$autori. "</div><div class='cella'>" .$costo. "</div>";
 				if ((isset($_GET['id']))&&($_GET['id']==$ISBN)) {
 					echo "<div class='cella'>";
-					echo "<form id='updatePrize' action='supportUpdate.php?id=".$ISBN."' method='POST'>";
+					echo "<form id='updatePrize' action='support2Update.php?id=".$ISBN."' method='POST'>";
 					echo "<input type='number' id='costo' name='costo' placeholder='Nuovo prezzo' class='form_input'>";
 					echo "</br>";
 					echo "<input type='submit' id='submit' name='submit' value='aggiorna' class='form_submit'>";
 					echo "</form>";
 					echo "</div></div>";
-				}	
+				}
+					
 				else {
-					echo "<div class='cella'><a href='allBooks.php?id=".$ISBN."'>aggiorna</a></div></div>";
+					echo "<div class='cella'><a href='all1Books.php?id=".$ISBN."'>aggiorna</a></div></div>";
 				}
 			}
 			echo "</div>";
@@ -61,6 +62,7 @@
 			require('comuni/footer.php');
 		}
 	}
+	
 ?>
 </body>
 </html>
