961609bb10faedf6c6c9910f60ef66e349c65e03
MrSpleen3 <93833269+MrSpleen3@users.noreply.github.com>
2021-12-29 10:53:19 +0100

    Update ban.php

--- areaAmministrativa/ban.php
+++ areaAmministrativa/ban.php
@@ -1,13 +1,27 @@
 <?php
+	function update_user($id,$con) {
+		
+		$up_query="UPDATE utente SET ban=1 WHERE utente.id='$id'";
+		$upuser=mysqli_query($con,$up_query);
+		if (mysqli_affected_rows($con) == 1) {
+			return 0;
+		}
+		else {
+			return 1;
+		}
+	}
+
+
 	session_start();
 	if (!isset($_SESSION["adminid"])){
-		header('Location: adiminLogin.php');
+		header('Location: login.php');
 		exit();
 	}
 	else{
 		$lastactivity=$_SESSION["last_activity"];
 		$lastactivity= time() - $lastactivity;
 		if($lastactivity > 1800) {
+			session_unset();
 			session_destroy();
 			header('Location:login.php');
 			exit();
@@ -16,21 +30,32 @@
 			$_SESSION["last_activity"]=time();
 			$id = $_GET['id'];
 			$adminid=$_SESSION["adminid"];
-			$pattern="/^[0-9]{11}$/";
+			$pattern="/^[0-9]{1,11}$/";
 			if (preg_match($pattern,$id)&&preg_match($pattern,$adminid)) {
-				$data=date("d/m/Y");
-				require('../DB_Connections/webadmin_access.php');
-				$conn= webadmin_access();
-				$query = "INSERT INTO ban (id_utente,id_amm,data) VALUES ('$id','$adminid','$data')";
-				$res = mysqli_query($conn,$query);
-				echo "utente bannato"; 
-				mysqli_close($conn);
-				header('Location: allUser.php');
-				exit();
+				$data=date("Y/m/d");
+				require('DB_connections/webadmin_access.php');	
+				$conn=webadmin_access();
+				$id=mysqli_real_escape_string($conn,$id);
+				$adminid=mysqli_real_escape_string($conn,$adminid);
+				$data=mysqli_real_escape_string($conn,$data);
+				$result=update_user($id,$conn);
+				if($result == 0){										//qua uguale a 0 perchè aff rows deve ritornarmi 1
+					$query = "INSERT INTO ban (id_utente,id_amm,data) VALUES ('$id','$adminid','$data')";
+					$res = mysqli_query($conn,$query);
+					echo "utente bannato"; 
+					mysqli_close($conn);
+					header('Location: allUsers.php');
+					exit();
+				}
+				else {
+					$_SESSION["errore"]="vi è stato un errore nel ban,ripetere l'operazione";
+					mysqli_close($conn);
+					header("Location: allUsers.php");
+					exit();
+				}
 			}
 			else {
-				$_SESSION_["errore"]="vi è stato un errore nel ban,ripetere l'operazione";
-				mysqli_close($conn);
+				$_SESSION["errore"]="vi è stato un errore nel ban,ripetere l'operazione";
 				header("Location: allUsers.php");
 				exit();
 			}
